43d84ed8dd1fc5e8f28822ad5452699251b3d645
jooojub <jooojub@gmail.com>
2019-06-13 22:55:08 +0900

    build: Add simple build scripts
    Signed-off-by: jooojub <jooojub@gmail.com>

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+node tools/build.js armasm asciidoc avrasm awk bash basic cmake cpp cs csp css diff dos dts http htmlbars ini java javascript json less makefile markdown mipsasm perl php python qml scss shell smali vim x86asm xml
+
+cp build/highlight.pack.js /home/jooojub/Workspace/github/jooojub.github.io/css/
